be16a89082828e10ddc4ff6cf39f1cff42464ad2
red-devils-united <dorian.urbaniak@wp.pl>
2025-12-30 18:48:48 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
-import Header from "../components/Header";
 import "../styles/globals.css";
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
