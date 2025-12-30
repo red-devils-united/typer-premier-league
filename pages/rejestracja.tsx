af664c7820b617d95bb6d0a369f81b009709b32c
red-devils-united <dorian.urbaniak@wp.pl>
2025-12-30 15:44:10 +0000

    Create rejestracja.tsx

---
+++ pages/rejestracja.tsx
@@ -0,0 +1,8 @@
+export default function Rejestracja() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Rejestracja</h1>
+      <p>Tu będzie formularz rejestracji 👤</p>
+    </main>
+  );
+}
